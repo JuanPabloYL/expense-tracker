00f8ca1f7efe82df76a3bcfe605e6a999c70e840
Juan Pablo Yagüe <juanpabloyague@gmail.com>
2025-02-25 20:33:16 -0800

    Handle graphic of expenses

--- src/components/BudgetTracker.tsx
+++ src/components/BudgetTracker.tsx
@@ -1,13 +1,26 @@
+import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
 import { useBudget } from "../hooks/useBudget";
 import AmountDisplay from "./AmountDisplay";
+import "react-circular-progressbar/dist/styles.css";
 
 export const BudgetTracker = () => {
   const { state, totalExpenses, remainingBudget } = useBudget();
 
+  const percentage = +((totalExpenses / state.budget) * 100).toFixed(2);
+
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
       <div className="flex justify-center">
-        <img src="/grafico.jpg" alt="Expenses chart" />
+        <CircularProgressbar
+          value={percentage}
+          styles={buildStyles({
+            pathColor: "#3b82f6",
+            trailColor: "#f5f5f5",
+            textSize: 8,
+            textColor: "#3b82f6",
+          })}
+          text={`${percentage}% Spent`}
+        />
       </div>
 
       <div className="flex flex-col justify-center items-center gap-8">
